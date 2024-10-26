0771c92c7c114023f264161ab6fc2c2700d1b00a
Tyler Shick <tylershick@gmail.com>
2024-10-26 10:53:52 -0400

    Database cleanup

--- lib/db/drizzle.ts
+++ lib/db/drizzle.ts
@@ -1,5 +1,5 @@
-import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
+import { drizzle } from 'drizzle-orm/postgres-js';
 import * as schema from './schema';
 import dotenv from 'dotenv';
 
